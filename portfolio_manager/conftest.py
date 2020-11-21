7eb3f153584e29e88880cded30c02e90c7654af3
mirceachira <chira.mircea.darius@gmail.com>
2020-11-21 09:02:43 +0200

    cookiecutter initial commit

---
+++ portfolio_manager/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from portfolio_manager.users.models import User
+from portfolio_manager.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
